08d55011d77e2315517d7deb583af36460b33b8a
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2021-03-10 17:04:54 +0000

    Added aditional files

--- Project_Template/scenebasic_uniform.h
+++ Project_Template/scenebasic_uniform.h
@@ -6,12 +6,22 @@
 #include <glad/glad.h>
 #include "helper/glslprogram.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "helper/torus.h";
+
 class SceneBasic_Uniform : public Scene
 {
 private:
-    GLuint vaoHandle;
+    //GLuint vaoHandle;
+
     GLSLProgram prog;
+
+    Torus torus;
     float angle;
+
+
 
     void compile();
 
